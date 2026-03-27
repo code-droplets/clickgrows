250d9466f55e2da08e9d03807f42a32843dad30b
code-droplets <harshkain46@gmail.com>
2026-03-28 00:50:19 +0530

    Change in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // or '/your-repo-name/'
+  base: '/clickgrows/',   // 🔥 THIS IS REQUIRED
 })
